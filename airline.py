6bc9492e116ce9246bd97081387b6d299f2792f8
zseid <samer.zeid@sjsu.edu>
2017-03-02 18:39:26 -0800

    Working
    +Twilio SMS feature added

--- airline.py
+++ airline.py
@@ -1,4 +1,5 @@
 import time
+import sys
 from selenium import webdriver
 from datetime import datetime
 from selenium.webdriver.common.keys import Keys
@@ -6,17 +7,19 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from twilio.rest import TwilioRestClient 
+
 
 class ExpediaScraper():
 
-    def main(self):
+    def main(self):  
         while True:
             self.searchsite()
-            time.sleep(3600) #Check prices once ever hour
+            time.sleep(3600) #try again every hour if deal isn't found
 
     def getargs(self):
         
-
+       
         self.departfrom = input("Flying From (e.g, SFO): ")
         self.flyto = input("Flying To: ")
         self.departdate = input("Departure Date (e.g, 03/02/2017): ")
@@ -35,8 +38,8 @@ class ExpediaScraper():
                     print("Still not in range. Try Again.")
         
         #Get max price
-        self.desiredprice = input("What's the maximum you'd like to spend in dollars? (e.g. 550): 
-                                  
+        self.desiredprice = input("What's the maximum you'd like to spend in dollars? (e.g. 550): ")
+
         self.searchsite()
     
     def searchsite(self):
@@ -84,18 +87,32 @@ class ExpediaScraper():
             finalprice = theprices.text
             price_array.append(finalprice[1:])
         min_price = min(price_array)
-        #compare the min of the array against our desired price
+
         if int(min_price) <= self.desiredprice:
             self.notifyuser(min_price)
         else:
             print("No match found in this run. We'll try again.")
-            driver.quit() #close the driver to prepare for the next attempt
+            driver.quit()
             return
 
     def notifyuser(self, min_price):
         current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         print("\nWe found a steal!", min_price,"for roundtrip from",self.departfrom,"to",self.flyto, current_time)
-        SystemExit() #our job is done here
+        #send twilio SMS message alert
+        # EDIT THESE SETTINGS
+        ACCOUNT_SID = "TWILIO ACCOUNT SID" 
+        AUTH_TOKEN = "TWILIO AUTH TOKEN"        
+        FROM_NUMBER = "TWILIO ASSIGNED NUMBER"
+        TO_NUMBER = "SEND DEAL UPDATE TO THIS NUMBER" 
+
+        client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN) 
+        
+        client.messages.create(
+        to=TO_NUMBER, 
+        from_=FROM_NUMBER, 
+        body=("Deal found. ",min_price)
+        )
+        sys.exit()
 
 
 if __name__ == "__main__":
